cc0f37239f4e1eba24bac0d0ffa36934b5adb8f0
peln <pern9378@stud.kea.dk>
2019-10-24 10:38:50 +0200

    Auto-commit

---
+++ weatherapp/precache-manifest.33e83e6ffdd77ef0bb4c01df20e06536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394ecc0d9f75f61ec3a4ff23204f0cad",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "8c100d2d075d1736f3cf",
+    "url": "/weatherapp/static/css/main.1ac08b2f.chunk.css"
+  },
+  {
+    "revision": "5ddf47322b0ce78d6158",
+    "url": "/weatherapp/static/js/2.79e08076.chunk.js"
+  },
+  {
+    "revision": "8c100d2d075d1736f3cf",
+    "url": "/weatherapp/static/js/main.2e759046.chunk.js"
+  },
+  {
+    "revision": "5a0da1122d01d072ae96",
+    "url": "/weatherapp/static/js/runtime-main.dc79da9a.js"
+  },
+  {
+    "revision": "926689af119db7f40f170f01e690372a",
+    "url": "/weatherapp/static/media/4k-wallpaper-air-atmosphere-2310713.926689af.jpg"
+  },
+  {
+    "revision": "9a8b8981bbe2a3ecf001ff1421828e0e",
+    "url": "/weatherapp/static/media/wathericonlogo.9a8b8981.png"
+  }
+]);
